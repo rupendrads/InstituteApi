1b01636e4c51ad652b2af303867b549f88bd5056
chittaranjan-dev <ranjan74feb@yahoo.co.in>
2023-04-05 18:43:06 +0530

    Added Royalty payout controller

---
+++ Controllers/RoyaltyPayoutController.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using InstituteApi.Models;
+using InstituteApi.DTOs;
+using Microsoft.AspNetCore.Cors;
+
+namespace InstituteApi.Controllers;
+
+[EnableCors("_myAllowSpecificOrigins")]
+[ApiController]
+[Route("api/[controller]")]
+public class RoyaltyPayoutController: ControllerBase 
+{
+    private readonly InstituteContext _context;
+
+    public RoyaltyPayoutController(InstituteContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/RoyaltyPayout?instituteId=1
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<RoyaltyDistributionDto>>> GetRoyaltyDistributions(long instituteId)
+    {
+        return await _context.RoyaltyDistributions.Include(rd => rd.Admission).Include(rd => rd.Admission.User)
+                    .Include(rd => rd.RoyaltyDistributionDetails).ThenInclude(rdd => rdd.User)
+                    .Where(rd => rd.Admission.InstituteId == instituteId && rd.RoyaltyDistributionDetails.Any(rdd => rdd.PayoutFlag == false))
+                    .Select(x => RoyaltyDistributionItemToDto(x))
+                    .ToListAsync();
+    }
+
+    private static RoyaltyDistributionDto RoyaltyDistributionItemToDto(RoyaltyDistribution royaltyDistribution)
+    {
+        RoyaltyDistributionDto royaltyDistributionDto = new RoyaltyDistributionDto {
+            Id = royaltyDistribution.Id,
+            AdmissionId = royaltyDistribution.AdmissionId,
+            DateOfAdmission = royaltyDistribution.Admission.DateOfAdmission,
+            UserId = royaltyDistribution.Admission.UserId,
+            FirstName = royaltyDistribution.Admission.User.FirstName,
+            LastName = royaltyDistribution.Admission.User.LastName,
+            DateOfExecution = royaltyDistribution.DateOfExecution
+        };
+
+        if(royaltyDistributionDto.RoyaltyDistributionDetails == null)
+        {
+            royaltyDistributionDto.RoyaltyDistributionDetails = new List<RoyaltyDistributionDetailDto>();
+        }
+
+        if(royaltyDistribution.RoyaltyDistributionDetails!= null)
+        {
+            royaltyDistribution.RoyaltyDistributionDetails.ToList().ForEach(rdd => {
+                royaltyDistributionDto.RoyaltyDistributionDetails.Add(new RoyaltyDistributionDetailDto {
+                    Id = rdd.Id,
+                    RoyaltyLevelDetailId = rdd.RoyaltyLevelDetailId,
+                    RoyaltyAmount = rdd.RoyaltyAmount,
+                    UserId = rdd.UserId,
+                    FirstName = rdd.User.FirstName,
+                    LastName = rdd.User.LastName,
+                    PayoutDate = rdd.PayoutDate,
+                    PayoutFlag = rdd.PayoutFlag
+                });
+            });
+        }
+        return royaltyDistributionDto;
+    }
+}
